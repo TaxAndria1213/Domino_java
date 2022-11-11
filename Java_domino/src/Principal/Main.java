a45a9516f24eaba11381083b6cd9226becf8f098
TaxAndria1213 <taxandria@gmail.com>
2022-11-11 10:02:04 +0300

    Création du projet avec les codes

---
+++ Java_domino/src/Principal/Main.java
@@ -0,0 +1,22 @@
+package Principal;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import Interface.Interface_global;
+import bin.Domino;
+import bin.Serveur;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		//Scanner entree = new Scanner(System.in);
+		//int nombreJoueur = entree.nextInt();
+		
+		
+		Serveur serveur = new Serveur();
+		//serveur.setNbJoueur(nombreJoueur);
+		
+
+	}
+}
