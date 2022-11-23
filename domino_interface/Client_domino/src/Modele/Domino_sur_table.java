caf417461bfd320b0d527ec874f978d5755f4d16
TaxAndria1213 <taxandria@gmail.com>
2022-11-23 23:46:21 +0300

    push code

--- domino_interface/Client_domino/src/Modele/Domino_sur_table.java
+++ domino_interface/Client_domino/src/Modele/Domino_sur_table.java
@@ -1,20 +1,20 @@
 package Modele;
 
+import java.awt.BorderLayout;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
-public class Domino_sur_table extends JPanel {
+public class Domino_sur_table extends JButton {
 	public String valeurPresser;
 	private String chemin;
 	private BufferedImage domi;
 	
 	public Domino_sur_table(String nom) {
-		
-		/*
 	try {
 			chemin = "./image_domino_png/"+nom+".png";
 			domi = ImageIO.read(new File(chemin));
@@ -22,13 +22,12 @@ public class Domino_sur_table extends JPanel {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-*/
 	}
 	
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.drawImage(domi, 140, 140, null);
+		g.drawImage(domi, 0, 0, null);
 	}
 }
 
